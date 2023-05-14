d3975cf7ebd5d272d003c31ddc611aa96a963297
Sandeep Singh <115461230+sandeepsinghh143@users.noreply.github.com>
2023-05-14 15:57:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function rot13(encodedStr){
    let decodedArr = []; // Your Result goes here
   // Only change code below this line
 	for(let i=0;i<encodedStr.length;i++){
-		if(encodedStr[i]==""){
+		if(encodedStr[i]==" "){
 			decodedArr.push(" ");
 		}else{
 			decodedArr.push(lookup.encodedStr[i]);
